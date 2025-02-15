a170b0e3c2c8ddc8dc6fb9514a04c5e656e0c22c
Talal-Hassan-Programmer <Talal-Hassan@outlook.com>
2025-02-15 15:58:30 +0300

    writiing main funcs in funcs.py

---
+++ funcs.py
@@ -0,0 +1,32 @@
+import random,string
+
+class Genrate_Password:
+    def __init__(self, lenght=12, use_upper=True, use_Digits=True, use_Special=True):
+        self.lenght = lenght
+        self.use_upper = use_upper
+        self.use_Digits = use_Digits
+        self.use_Special = use_Special
+
+    def generate_password(self):
+        lower = string.ascii_lowercase
+        upper = string.ascii_uppercase if self.use_upper else ''
+        digits = string.digits if self.use_Digits else ''
+        special = string.punctuation if self.use_Special else ''
+
+        all = lower + upper + digits + special
+
+        password = ''.join(random(all) for _ in range(self.lenght)) 
+        
+
+        if not all:
+            return 'Error: No characters to choose from'
+
+
+        return password
+
+
+    def save_Password(self, password):
+        with open('password.txt', 'a') as file:
+            file.write(password)
+
+        return 'Password saved to password.txt'
